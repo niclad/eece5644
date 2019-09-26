48484013d4d128bfe0f8a34eda9e0daad36455ef
niclad <kingwoodnt@aol.com>
2019-09-26 16:45:14 -0400

    updated files

--- homework-1/q4p2.m
+++ homework-1/q4p2.m
@@ -2,7 +2,9 @@
 % 9/29/2019
 
 % decision rule
-syms x u s
+syms x
+u = 1;
+s = sqrt(2);
 x1 = -(u + s*((u^2) + (2*log(s)*(s^2 - 1)))^(1/2))/(s^2 - 1);
 x2 = -(u - s*((u^2) + (2*log(s)*(s^2 - 1)))^(1/2))/(s^2 - 1);
 
@@ -15,9 +17,28 @@ p1x = px1 / (px1 + px2);
 p2x = px2 / (px1 + px2);
 
 pRange = [-6 6];
+figure(1)
 hold on
 fplot(px1, pRange, '--b')
 fplot(px2, pRange, '--r')
+plot([x1 x1], [0 1], ':k')
+plot([x2 x2], [0 1], ':k')
+ylim([0 0.4])
+title({'Decision Boundary for Likelihoods'})
+xlabel('Distance from mean')
+ylabel('P(x)')
+legend('P(x|L=1)','P(x|L=2)', 'Decision Boundary')
+hold off
+print -depsc plot1.eps
+figure(2)
+hold on
 fplot(p1x, pRange, '-g')
 fplot(p2x, pRange, '-m')
-legend('P(x|L=1)','P(x|L=2)','P(L=1|x)','P(L=2|x)')
+plot([x1 x1], [0 1], ':k')
+plot([x2 x2], [0 1], ':k')
+ylim([0 1])
+title('Posterior Probabilities for Two-Class Setting')
+xlabel('Distance from mean')
+ylabel('P(x)')
+legend('P(L=1|x)','P(L=2|x)', 'Decision Boundary')
+print -depsc plot2.eps
