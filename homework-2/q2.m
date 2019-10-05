50e085058ea720fbad8335ba63c1c576ae64aa04
niclad <kingwoodnt@aol.com>
2019-10-05 13:17:02 -0400

    added hw 2 folder

---
+++ homework-2/q2.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = q2(samples)
+% Generates a specified number of IID samples
+%   Taking in a specified number of samples, the function returns a number
+%   of IID samples paired with class labels
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
